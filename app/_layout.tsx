96a51c9bfd49793e44136eda48f931bf11d8ab88
Nirman Shrestha <nirman@prixatech.com>
2025-09-24 22:53:52 +0545

    disabled edge to edge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,34 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <>
-      <SafeAreaProvider>
-        <SafeAreaView className="flex-1">
-          <Stack
-            screenOptions={{
-              headerShown: true,
-              headerStyle: { backgroundColor: "red" },
-              headerTransparent: false,
-            }}
-          >
-            <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
-            <Stack.Screen
-              name="movie/[id]"
-              options={({
-                route,
-              }: {
-                route: { params?: { title?: string } };
-              }) => ({
-                title: route?.params?.title ?? "Movie",
-              })}
-            />
-            <Stack.Screen
-              name="tv/[id]"
-              options={({
-                route,
-              }: {
-                route: { params?: { title?: string } };
-              }) => ({
-                title: route?.params?.title ?? "TV Show",
-              })}
-            />
-          </Stack>
-        </SafeAreaView>
-      </SafeAreaProvider>
-      <StatusBar barStyle={"default"} translucent={false} />
-    </>
+    <SafeAreaProvider>
+      <StatusBar style="auto" translucent={false} />
+      <Stack
+        screenOptions={{
+          headerShown: true,
+          headerStyle: { backgroundColor: "red" },
+          headerTransparent: false,
+          headerTitleAlign: "center",
+        }}
+      >
+        <Stack.Screen name="(tabs)" options={{ title: "Chrome" }} />
+        <Stack.Screen
+          name="movie/[id]"
+          options={({ route }: { route: { params?: { title?: string } } }) => ({
+            title: route?.params?.title ?? "Movie",
+          })}
+        />
+        <Stack.Screen
+          name="tv/[id]"
+          options={({ route }: { route: { params?: { title?: string } } }) => ({
+            title: route?.params?.title ?? "TV Show",
+          })}
+        />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
